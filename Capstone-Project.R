6321ad2ae270e923e2974df9c3a00160e70e83bb
Kaushik <kaushik.sivasankaran@ab-ovo.com>
2021-08-08 17:33:01 -0400

    Commit x

--- Capstone-Project.R
+++ Capstone-Project.R
@@ -2,22 +2,23 @@
 library(dplyr)
 library(ggplot2)
 library(tm)
+library(RWeka)
+library(SnowballC)
+library(wordcloud)
+library(ngram)
+library(textstem)
 
-# Create connections and load the data
+# Load the data
 
-con_blog <- file("./Coursera-SwiftKey/final/en_US/en_US.blogs.txt","r")
-con_news <- file("./Coursera-SwiftKey/final/en_US/en_US.news.txt","r")
-con_twitter <- file("./Coursera-SwiftKey/final/en_US/en_US.twitter.txt","r")
 
 en_US_blog <- readLines("./Coursera-SwiftKey/final/en_US/en_US.blogs.txt")
 en_US_news <- readLines("./Coursera-SwiftKey/final/en_US/en_US.news.txt")
 en_US_twitter <- readLines("./Coursera-SwiftKey/final/en_US/en_US.twitter.txt")
 
 
+# Get basic information about the files
 
-#Get basic information about the files
-
-#Word counts
+## Word counts
 
 Wordcount_blog <- wordcount(en_US_blog, sep = " ", count.function = sum)
 
@@ -26,7 +27,7 @@ Wordcount_news <- wordcount(en_US_news, sep = " ", count.function = sum)
 Wordcount_twitter <- wordcount(en_US_twitter, sep = " ", count.function = sum)
 
 
-# Length 
+## Length 
 
 Length_blog <- length(en_US_blog)
 
@@ -34,7 +35,7 @@ Length_news <- length(en_US_news)
 
 Length_twitter <- length(en_US_twitter)
 
-# File size
+## File size
 
 Size_blog <- file.info("./Coursera-SwiftKey/final/en_US/en_US.blogs.txt")$size / (1024*1024)
 
@@ -42,7 +43,7 @@ Size_news <- file.info("./Coursera-SwiftKey/final/en_US/en_US.news.txt")$size / 
 
 Size_twitter <- file.info("./Coursera-SwiftKey/final/en_US/en_US.twitter.txt")$size / (1024*1024)
 
-# Summarry
+## Summarry
 
 Data_summary <- data.frame(c("Blog", "News", "Twitter"),
                            c(Wordcount_blog,Wordcount_news,Wordcount_twitter),
@@ -65,55 +66,49 @@ twitter_sample <- en_US_twitter[sample(1:length(en_US_twitter), 10000)]
 
 sample_data <- c(blog_sample, news_sample, twitter_sample)
 
-writeLines(sample_data, "./sample_data.txt")
+writeLines(sample_data, "./Sample/sample_data.txt")
 
 # Remove temporary variables
 
-rm(en_US_blog, en_US_news, en_US_twitter, blog_sample, news_sample, twitter_sample)
+rm(en_US_blog, en_US_news, en_US_twitter, blog_sample, news_sample, twitter_sample, Length_blog,
+   Length_news, Length_twitter, Size_blog, Size_news, Size_twitter, Wordcount_blog, Wordcount_news,
+   Wordcount_twitter)
 
 
 # Create Corpus
 
 docs <- Corpus(DirSource('./Sample'))
 
+# Remove URL, Twitter handles and email patterns
+
+toSpace <- content_transformer(function(x, pattern) gsub(pattern, " ", x))
+
+docs <- tm_map(docs, toSpace, "(f|ht)tp(s?)://(.*)[.][a-z]+")
+docs <- tm_map(docs, toSpace, "@[^\\s]+")
+docs <- tm_map(docs, toSpace, "\\b[A-Z a-z 0-9._ - ]*[@](.*?)[.]{1,3} \\b")
+
+# # Remove profane words
+# 
+# ## Download profane words file
+# 
+# badWordsURL <- "http://www.idevelopment.info/data/DataScience/uploads/full-list-of-bad-words_text-file_2018_07_30.zip"
+# badWordsFile <- "data/full-list-of-bad-words_text-file_2018_07_30.txt"
+# if (!file.exists('data')) {
+#   dir.create('data')
+# }
+# if (!file.exists(badWordsFile)) {
+#   tempFile <- tempfile()
+#   download.file(badWordsURL, tempFile)
+#   unzip(tempFile, exdir = "data")
+#   unlink(tempFile)
+# }
 
 # Convert to lowercase
 
 docs <- tm_map(docs, content_transformer(tolower))
 
-# Transformations
+# Other Transformations
 
-toSpace <- content_transformer(function(x, pattern){return (gsub(pattern, " ", x))})
-
-# docs <- tm_map(docs, toSpace, "-")
-# docs <- tm_map(docs, toSpace, ":")
-# #docs <- tm_map(docs, toSpace, "'")
-# docs <- tm_map(docs, toSpace, ";")
-# docs <- tm_map(docs, toSpace, ",")
-# docs <- tm_map(docs, toSpace, "<")
-# docs <- tm_map(docs, toSpace, ".")
-# docs <- tm_map(docs, toSpace, ">")
-# docs <- tm_map(docs, toSpace, "/")
-# docs <- tm_map(docs, toSpace, "?")
-# docs <- tm_map(docs, toSpace, "\\[")
-# docs <- tm_map(docs, toSpace, "\\{")
-# docs <- tm_map(docs, toSpace, "\\]")
-# docs <- tm_map(docs, toSpace, "\\}")
-# #docs <- tm_map(docs, toSpace, "\\")
-# docs <- tm_map(docs, toSpace, "\\|")
-# docs <- tm_map(docs, toSpace, "!")
-# docs <- tm_map(docs, toSpace, "@")
-# docs <- tm_map(docs, toSpace, "#")
-# docs <- tm_map(docs, toSpace, "$")
-# docs <- tm_map(docs, toSpace, "%")
-# docs <- tm_map(docs, toSpace, "^")
-# docs <- tm_map(docs, toSpace, "&")
-# docs <- tm_map(docs, toSpace, "\\*")
-# docs <- tm_map(docs, toSpace, "\\(")
-# docs <- tm_map(docs, toSpace, "\\)")
-# docs <- tm_map(docs, toSpace, "_")
-# docs <- tm_map(docs, toSpace, "\\+")
-# docs <- tm_map(docs, toSpace, "=")
 
 docs <- tm_map(docs, toSpace, "/|@|\\|")
 
@@ -132,3 +127,98 @@ docs <- tm_map(docs, stripWhitespace)
 # Remove stop words
 
 docs <- tm_map(docs, removeWords, stopwords("english"))
+
+# Perform lemmatization
+
+docs <-tm_map(docs, lemmatize_strings)
+
+docs <- tm_map(docs, PlainTextDocument)
+
+# N-gram tokenization
+
+## Unigram toeknization
+token_uni <- function(x) NGramTokenizer(x, Weka_control(min = 1, max = 1))
+
+documentMatrix_uni <- DocumentTermMatrix(docs, control = list(tokenize = token_uni))
+
+## Bigram tokenization
+token_bi <- function(x) NGramTokenizer(x, Weka_control(min = 2, max = 2))
+
+documentMatrix_bi<- DocumentTermMatrix(docs, control = list(tokenize = token_bi))
+
+## Trigram tokenization
+token_tri <- function(x) NGramTokenizer(x, Weka_control(min = 3, max = 3))
+
+documentMatrix_tri <- DocumentTermMatrix(docs, control = list(tokenize = token_tri))
+
+# Unigram Exploratory data analysis
+
+## Calculate frequency of words
+freq_uni <- sort(colSums(as.matrix(documentMatrix_uni)), decreasing = TRUE)
+
+## Create histogram of unigrams with frequency > 1000
+
+df_uni <- data.frame(word =  names(freq_uni), frequency = freq_uni)
+
+df_uni %>% filter(frequency > 1000) %>%
+            ggplot(aes(word, frequency)) +
+            geom_bar(stat = "identity") +
+            ggtitle("Unigrams with frequencies > 1000") +
+            xlab("Unigrams") + ylab("Frequency") +
+            theme(axis.text.x = element_text(angle = 45, hjust = 1), plot.title = element_text(hjust = 0.5))
+
+## Create wordcloud of unigrams with frequency > 1000
+
+set.seed(15)
+
+wordcloud(names(freq_uni), freq_uni, min.freq = 1000, colors = brewer.pal(6, "Dark2"))
+
+
+
+# Bigram Exploratory data analysis
+
+## Calculate frequency of words
+freq_bi <- sort(colSums(as.matrix(documentMatrix_bi)), decreasing = TRUE)
+
+
+## Create histogram of unigrams with frequency > 100
+
+df_bi <- data.frame(word =  names(freq_bi), frequency = freq_bi)
+
+df_bi %>% filter(frequency > 100) %>%
+  ggplot(aes(word, frequency)) +
+  geom_bar(stat = "identity") +
+  ggtitle("Bigrams with frequencies > 100") +
+  xlab("Bigrams") + ylab("Frequency") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1), plot.title = element_text(hjust = 0.5))
+
+## Create wordcloud of bigrams with frequency > 100
+
+set.seed(45)
+
+wordcloud(names(freq_bi), freq_bi, min.freq = 100, colors = brewer.pal(6, "Dark2"))
+
+# Trigram Exploratory data analysis
+
+## Calculate frequency of words
+freq_tri <- sort(colSums(as.matrix(documentMatrix_tri)), decreasing = TRUE)
+
+
+## Create histogram of trigrams with frequency > 10
+
+df_tri <- data.frame(word =  names(freq_tri), frequency = freq_tri)
+
+df_tri %>% filter(frequency > 10) %>%
+  ggplot(aes(word, frequency)) +
+  geom_bar(stat = "identity") +
+  ggtitle("Trigrams with frequencies > 10") +
+  xlab("Trigrams") + ylab("Frequency") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1), plot.title = element_text(hjust = 0.5))
+
+## Create wordcloud of bigrams with frequency > 10
+
+set.seed(45)
+
+wordcloud(names(freq_tri), freq_tri, min.freq = 10, colors = brewer.pal(6, "Dark2"))
+
+
