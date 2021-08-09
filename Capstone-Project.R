d53fe4b7ca862d6d89963d4f60a44e70aa26d5bf
Kaushik <kaushik.sivasankaran@ab-ovo.com>
2021-08-09 11:24:48 -0400

    commit

--- Capstone-Project.R
+++ Capstone-Project.R
@@ -1,4 +1,4 @@
-#Load libraries
+# Load libraries
 library(dplyr)
 library(ggplot2)
 library(tm)
@@ -115,32 +115,29 @@ docs <- tm_map(docs, PlainTextDocument)
 # N-gram tokenization
 
 ## Unigram toeknization
-UnigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 1, max = 1))
-unidtm <- DocumentTermMatrix(docs, 
-                             control = list(tokenize = UnigramTokenizer))
+Unigram <- NGramTokenizer(docs, Weka_control(min = 1, max = 1))
+
 
 ## Bigram tokenization
-BigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 2, max = 2))
-bidtm <- DocumentTermMatrix(docs, 
-                            control = list(tokenize = BigramTokenizer))
+Bigram <- NGramTokenizer(docs, Weka_control(min = 2, max = 2)) 
+
 
 ## Trigram tokenization
-TrigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 3, max = 3))
-tridtm <- DocumentTermMatrix(docs, 
-                             control = list(tokenize = TrigramTokenizer))
+Trigram <- NGramTokenizer(docs, Weka_control(min = 3, max = 3)) 
+
 
 # Unigram Exploratory data analysis
 
-## Calculate frequency of words
-tm_unifreq <- sort(colSums(as.matrix(unidtm)), decreasing=TRUE)
 
 ## Create histogram of unigrams with frequency > 1000
 
-tm_uniwordfreq <- data.frame(word=names(tm_unifreq), freq=tm_unifreq)
+df_uniwordfreq <- data.frame(table(Unigram))
 
-tm_uniwordfreq %>% 
-  filter(freq > 1000) %>%
-  ggplot(aes(word,freq)) +
+df_uniwordfreq <- df_uniwordfreq[order(df_uniwordfreq$Freq, decreasing = TRUE),]
+
+df_uniwordfreq %>% 
+  filter(Freq > 1000) %>%
+  ggplot(aes(Unigram,Freq)) +
   geom_bar(stat="identity") +
   ggtitle("Unigrams with frequencies > 1000") +
   xlab("Unigrams") + ylab("Frequency") +
@@ -151,35 +148,31 @@ tm_uniwordfreq %>%
 
 set.seed(15)
 
-wordcloud(names(tm_unifreq), tm_unifreq, max.words=50, colors = brewer.pal(6, "Dark2"))
+wordcloud(df_uniwordfreq$Unigram, df_uniwordfreq$Freq, max.words=50, colors = brewer.pal(6, "Dark2"))
 
 
 # Bigram Exploratory data analysis
 
-## Calculate frequency of words
-tm_bifreq <- sort(colSums(as.matrix(bidtm)), decreasing=TRUE)
+## Create histogram of unigrams with frequency > 100
 
-## Create histogram of bigrams with frequency > 100
+df_biwordfreq <- data.frame(table(Bigram))
 
-tm_biwordfreq <- data.frame(word=names(tm_bifreq), freq=tm_bifreq)
+df_biwordfreq <- df_biwordfreq[order(df_biwordfreq$Freq, decreasing = TRUE),]
 
-tm_biwordfreq %>% 
-  filter(freq > 100) %>%
-  ggplot(aes(word,freq)) +
+df_biwordfreq %>% 
+  filter(Freq > 100) %>%
+  ggplot(aes(Bigram,Freq)) +
   geom_bar(stat="identity") +
   ggtitle("Bigrams with frequencies > 100") +
   xlab("Bigrams") + ylab("Frequency") +
   theme(plot.title = element_text(size = 14, hjust = 0.5),
         axis.text.x=element_text(angle=45, hjust=1))
 
-
-## Create wordcloud of top 50 bigrams 
+## Create wordcloud of top 50 unigrams
 
 set.seed(45)
 
-wordcloud(names(tm_biwordfreq), tm_biwordfreq, max.words=50, colors = brewer.pal(6, "Dark2"))
-
-
+wordcloud(df_biwordfreq$Bigram, df_biwordfreq$Freq, max.words=50, colors = brewer.pal(6, "Dark2"))
 # Trigram Exploratory data analysis
 
 ## Calculate frequency of words
@@ -187,21 +180,31 @@ tm_trifreq <- sort(colSums(as.matrix(tridtm)), decreasing=TRUE)
 
 ## Create histogram of trigrams with frequency > 10
 
-tm_triwordfreq <- data.frame(word=names(tm_trifreq), freq=tm_trifreq)
+## Create histogram of trigrams with frequency > 10
 
-tm_triwordfreq %>% 
-  filter(freq > 10) %>%
-  ggplot(aes(word,freq)) +
+df_triwordfreq <- data.frame(table(Trigram))
+
+df_triwordfreq <- df_triwordfreq[order(dftriwordfreq$Freq, decreasing = TRUE),]
+
+df_triwordfreq %>% 
+  filter(Freq > 10) %>%
+  ggplot(aes(Trigram,Freq)) +
   geom_bar(stat="identity") +
   ggtitle("Trigrams with frequencies > 10") +
   xlab("Trigrams") + ylab("Frequency") +
   theme(plot.title = element_text(size = 14, hjust = 0.5),
         axis.text.x=element_text(angle=45, hjust=1))
 
-## Create wordcloud of top 50 trigrams
+## Create wordcloud of top 50 unigrams
 
 set.seed(75)
 
-wordcloud(names(tm_trifreq), tm_trifreq, max.words=50, colors = brewer.pal(6, "Dark2"))
+wordcloud(df_triwordfreq$Trigram, df_triwordfreq$Freq, max.words=50, colors = brewer.pal(6, "Dark2"))
+
+one_gram <- NGramTokenizer(docs, Weka_control(min = 1, max = 1))
+
+two_gram <- NGramTokenizer(docs, Weka_control(min = 2, max = 2)) 
+
+three_gram <- NGramTokenizer(docs, Weka_control(min = 3, max = 3)) 
 
 
